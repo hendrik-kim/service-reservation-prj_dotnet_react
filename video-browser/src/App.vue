a9a88ee23de73a657035af1d2c5e4336b2cb15bd
hank.dohwankim <hank.dohwankim@gmail.com>
2020-12-31 17:09:55 -0600

    add Youtube API

--- video-browser/src/App.vue
+++ video-browser/src/App.vue
@@ -1,26 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <SearchBar @termChange="onTermChange" />
+    <VideoList />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    SearchBar,
+    VideoList,
+  },
+  methods: {
+    onTermChange(searchTerm) {
+      axios
+        .get('https://www.googleapis.com/youtube/v3/search', {
+          params: {
+            key: process.env.VUE_APP_API_KEY,
+            type: 'video',
+            part: 'snippet',
+            q: searchTerm,
+          },
+        })
+        .then((response) => console.log(response));
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ video-browser/src/components/SearchBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div><input @input="onInput" /></div>
+</template>
+
+<script>
+export default {
+  name: 'SearchBar',
+  methods: {
+    onInput: function(evt) {
+      this.$emit('termChange', evt.target.value);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
