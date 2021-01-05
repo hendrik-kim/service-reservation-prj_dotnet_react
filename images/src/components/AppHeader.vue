0fe42aa0a7d221997fa8d6e6b7c8803deb3bbb17
Hendrik Kim <48870515+hendrik-kim@users.noreply.github.com>
2021-01-04 22:35:53 -0600

    store token in local storage

--- images/src/components/AppHeader.vue
+++ images/src/components/AppHeader.vue
@@ -4,19 +4,30 @@
       Image Storage
     </a>
     <div class="right menu">
-      <a href="#" class="ui item" @click="login">Login</a>
+      <div v-if="isLoggedIn" class="horizontal">
+        <a class="item" href="#">Galleries</a>
+        <a class="item" href="#">Upload</a>
+        <a class="item" href="#">Logout</a>
+      </div>
+      <a v-else href="#" class="ui item" @click="login">Login</a>
     </div>
   </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import { mapActions, mapGetters } from 'vuex';
 
 export default {
   name: 'AppHeader',
+  computed: mapGetters(['isLoggedIn']),
   methods: mapActions(['login']),
   // methods: { ...mapActions(['login']), onLogClick() {} },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.horizontal {
+  display: flex;
+  flex-direction: row;
+}
+</style>
